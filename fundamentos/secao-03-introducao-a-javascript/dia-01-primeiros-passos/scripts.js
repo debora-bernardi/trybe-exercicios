2b643a02739415d268432d05b891c16324b277c1
Debora Bernardi <debora.e.v.bernardi@hotmail.com>
2023-02-27 19:49:02 -0300

    Exercício dia 01

--- fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos/scripts.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos/scripts.js
@@ -1,8 +1,33 @@
-let grade = 80;
-if (grade >= 80) {
-    console.log("Parabéns. você faz parte das pessoas aprovadas!");
-} else if (grade < 80 && grade >= 60) {
-    console.log("você esta na lisat de espera.");
-} else if (grade < 60) {
-    console.log("infelizmente você reprovou.");
+let a = 20;
+let b = 6;
+let c = 100;
+
+console.log("soma: "  + (a + b));
+console.log("subtração: " + (a - b));
+console.log("multiplicação: " + (a * b));
+console.log("divisão: " + (a / b) );
+console.log("módulo: " + (a % b));
+
+if (a > b) {
+    console.log("'a' é maior que 'b'");
+} else {
+    console.log("'b' é maior que 'a'");
+}
+
+if (a > b && a > c) {
+    console.log("O maior número é: " + a + " (a)");
+} else if (b > a && b > c) {
+    console.log("o maior número é: " + b + " (b)");
+} else {
+console.log("O maior número é: " + c + " (c)");
+}
+
+let number = 15;
+
+if (number > 0) {
+    console.log("positive");
+} else if (number < 0) {
+    console.log("negative");
+} else {
+    console.log("zero")
 }
