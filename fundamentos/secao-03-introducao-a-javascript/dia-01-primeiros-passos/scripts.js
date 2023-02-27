b2752ef6e196626501da2577792bcc1c65e275ac
Debora Bernardi <debora.e.v.bernardi@hotmail.com>
2023-02-27 16:27:45 -0300

    exercício03

--- fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos/scripts.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos/scripts.js
@@ -1,8 +1,8 @@
-let base = 5;
-let heigth = 8;
-let area = base * heigth;
-
-console.log(area);
-
-let perimeter = 5 + 5 + 8 + 8;
-console.log(perimeter);
+let grade = 80;
+if (grade >= 80) {
+    console.log("Parabéns. você faz parte das pessoas aprovadas!");
+} else if (grade < 80 && grade >= 60) {
+    console.log("você esta na lisat de espera.");
+} else if (grade < 60) {
+    console.log("infelizmente você reprovou.");
+}
